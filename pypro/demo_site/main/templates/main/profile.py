a6fd14ae1786e77a12857f5c8ab1c4dd9a68f517
Elchin Abdullayev <88291146+SuperUserAZ@users.noreply.github.com>
2023-08-11 17:59:01 +0400

    Update profile.py

--- pypro/demo_site/main/templates/main/profile.py
+++ pypro/demo_site/main/templates/main/profile.py
@@ -1,1 +1,17 @@
+{% extends 'main/layout.html' %}
 
+{% block title %}
+    Profile
+{% endblock %}
+
+{% block profilebody %}
+
+<main>
+    <section class="profile-wrapper">
+        <form action="models.py" method="get">
+            
+        </form>
+    </section>
+</main>
+
+{% endblock %}
