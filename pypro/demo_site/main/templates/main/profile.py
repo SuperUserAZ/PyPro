07d5b3c9eddad2e264f6f5cbf96c2c8bb72f41cb
Elchin Abdullayev <88291146+SuperUserAZ@users.noreply.github.com>
2023-08-14 18:01:39 +0400

    Update profile.py

--- pypro/demo_site/main/templates/main/profile.py
+++ pypro/demo_site/main/templates/main/profile.py
@@ -1,17 +1,30 @@
 {% extends 'main/layout.html' %}
 
 {% block title %}
-    Profile
+    Profile Page
 {% endblock %}
 
-{% block profilebody %}
+{% block bodypage %}
+<div class="profile-box">
+  <form action="" method="get">
+      <h1>Profile Information</h1>
 
-<main>
-    <section class="profile-wrapper">
-        <form action="models.py" method="get">
-            
-        </form>
-    </section>
-</main>
+      <div class="input-box">
+          <input type="text" required enabled maxlength="30">
+      </div>
 
+      <div class="input-box">
+          <input type="text" required enabled maxlength="30">
+      </div>
+      <div class="input-box">
+          <input type="email" required enabled maxlength="30">
+      </div>
+
+      <div class="input-box">
+          <input type="password" required enabled maxlength="20">
+      </div>
+
+      <center><button type="submit"><span>Save</span></button></center>
+  </form>
+</div>    
 {% endblock %}
