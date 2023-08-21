d96a7f2a1fe3321eea24b7b1b4c2ec15ae700626
Elchin Abdullayev <88291146+SuperUserAZ@users.noreply.github.com>
2023-08-21 17:32:44 +0400

    Update models.py

--- pypro/demo_site/main/models.py
+++ pypro/demo_site/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+class SiteUser(models.Model):
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    email = models.EmailField(unique=True)
+    us_psw = models.CharField(max_length=30, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(defautl=True)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.first_name + self.last_name
+
